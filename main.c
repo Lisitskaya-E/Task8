389951e7a9ad6858546848e6e2247f6a9f0f30c5
Stepan Migunov <74508616+stepan-migunov@users.noreply.github.com>
2020-12-08 02:52:04 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,63 +3,63 @@
 #include <locale.h>
 void swap(int*a, int*b)
 {
-    int t=*a;
-    *a=*b;
-    *b=t;
+    int t=*a ;
+    *a=*b ;
+    *b=t ;
 }
 
-int p1, s1;
+int p1, s1 ;
 void qs(int* a, int l, int r)
 {
 
-    int i = l, j = r, x = a[(l + r) / 2];
+    int i = l, j = r, x = a[(l + r) / 2] ;
 
     do
     {
         while (a[i] < x) {
-                i++;
-                s1+=1;
-        }s1+=1;
+                i++ ;
+                s1+=1 ;
+        }s1+=1 ;
         while (a[j] > x) {
-                j--;
-                s1+=1;
-        }s1+=1;
+                j-- ;
+                s1+=1 ;
+        }s1+=1 ;
 
         if(i <= j)
         {
 
             if (a[i] > a[j])
             {
-                p1=p1+1;
+                p1=p1+1 ;
 
 
-                swap(&a[i], &a[j]);
+                swap(&a[i], &a[j]) ;
             }
-            i++;
-            j--;
-            s1 =s1+1;
+            i++ ;
+            j-- ;
+            s1 =s1+1 ;
         }
     }
-    while (i <= j);
+    while (i <= j) ;
 
     if (i < r)
-        qs(a, i, r);
+        qs(a, i, r) ;
     if (l < j)
-        qs(a, l, j);
+        qs(a, l, j) ;
 }
 
 
 void Sort1(int* m, int n)
 {
-    for(int i = n - 1; i >= 1; i--)
-        for(int j = 0; j < i; j++)
+    for(int i = n - 1 ; i >= 1 ; i--)
+        for(int j = 0 ; j < i ; j++)
         {
             if(m[j] > m[j+1])
             {
-                p1 = p1+1;
-                swap(&m[j], &m[j+1]);
+                p1 = p1+1 ;
+                swap(&m[j], &m[j+1]) ;
             }
-            s1=s1+1;
+            s1=s1+1 ;
         }
 }
 int main()
@@ -71,35 +71,35 @@ int main()
     while (1)
     {
         int N;
-        printf("Введите номер сортировки: 1. Пузырьком 2. QuickSort  \n");
-        scanf("%d", &N);
+        printf("Г‚ГўГҐГ¤ГЁГІГҐ Г­Г®Г¬ГҐГ° Г±Г®Г°ГІГЁГ°Г®ГўГЄГЁ: 1. ГЏГіГ§Г»Г°ГјГЄГ®Г¬ 2. QuickSort  \n") ;
+        scanf("%d", &N) ;
 
-        int n, i;
-        p1=0;
-        s1=0;
-        printf("Введите количество элементов\n ");
-        scanf("%d", &n);
-        int* a;
-        a = (int*) malloc(n*sizeof(int));
-        printf("Введите массив\n ");
-        for (i=0; i<n; i++)
+        int n, i ;
+        p1=0 ;
+        s1=0 ;
+        printf("Г‚ГўГҐГ¤ГЁГІГҐ ГЄГ®Г«ГЁГ·ГҐГ±ГІГўГ® ГЅГ«ГҐГ¬ГҐГ­ГІГ®Гў\n ") ;
+        scanf("%d", &n) ;
+        int* a ;
+        a = (int*) malloc(n*sizeof(int)) ;
+        printf("Г‚ГўГҐГ¤ГЁГІГҐ Г¬Г Г±Г±ГЁГў\n ") ;
+        for (i=0 ; i<n ; i++)
         {
-            scanf("%d", a+i);
+            scanf("%d", a+i) ;
         }
         if (N==1)
         {
-            Sort1(a, n);
+            Sort1(a, n) ;
         }
         if (N==2)
         {
-            qs(a, 0, n-1);
+            qs(a, 0, n-1) ;
         }
 
-        for (i=0; i<n; i++)
+        for (i=0 ; i<n ; i++)
         {
-            printf("%3d ", a[i]);
+            printf("%3d ", a[i]) ;
         }
-        printf("\nсравнений   %4d перестановок  %4d\n", s1, p1);
+        printf("\nГ±Г°Г ГўГ­ГҐГ­ГЁГ©   %4d ГЇГҐГ°ГҐГ±ГІГ Г­Г®ГўГ®ГЄ  %4d\n", s1, p1) ;
 
     }
 }
